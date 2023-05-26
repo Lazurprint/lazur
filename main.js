9db17a5584726c26ad8db65c23f46f3c83caa0d7
Отдел допечатной подготовки <ctp@lazurprint.ru>
2023-05-26 19:22:01 +0500

    add

--- main.js
+++ main.js
@@ -188,6 +188,29 @@ modal.addEventListener('click',(e)=> {
     }
 })
 
+// Карта 
+
+ymaps.ready(init);
+
+function init() {
+
+    var myMap = new ymaps.Map("map", {
+
+        center: [57.348307, 61.405098],
+        zoom: 15,
+        controls: [],
+    });
+    var myPlacemark = new ymaps.Placemark([57.348307, 61.405098], {}, {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/marker.svg',
+        iconImageSize: [30, 30],
+        iconImageOffset: [0, 0],
+    });
+
+    myMap.geoObjects.add(myPlacemark);
+
+    myMap.behaviors.disable(['drag', 'rightMouseButtonMagnifier', 'scrollZoom']);
+};
 
 // Плавный скролл 
 
